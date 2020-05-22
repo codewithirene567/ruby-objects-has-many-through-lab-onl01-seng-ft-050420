1c295824fd81ba7223a5aac18f81caad178019fe
Irene Scott <irenejinscott@gmail.com>
2020-05-22 17:57:08 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,16 +2,18 @@ class Appointment
   
   @@all = []
   
-  def self.all
-    @@all
-  end 
+  
   
   def initialize(date, patient, doctor)
-    @date = ""
+    @date = date
     @patient = patient
     @doctor = doctor
     @@all << self 
   end
+
+  def self.all
+    @@all
+  end 
   
 end
     
