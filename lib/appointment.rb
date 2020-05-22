aa3367ed1b89298fb908dde09ecd55cc6a1261cc
Irene Scott <irenejinscott@gmail.com>
2020-05-22 18:03:16 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,19 +1,13 @@
 class Appointment
-attr_accessor :date, :patient, :doctor
-
+  attr_accessor :date, :patient, :doctor
   @@all = []
-
-
-
-  def initialize( date, patient, doctor) #why was it not equal to a string
+  def initialize( date, patient, doctor)
     @date = date
     @patient = patient
     @doctor = doctor
     @@all << self
   end
-
   def self.all
     @@all
   end
-
 end
