bf24b12b63d48642786f2d26849f0da4a316da44
Irene Scott <irenejinscott@gmail.com>
2020-05-22 17:37:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,13 +2,15 @@ class Patient
 attr_accessor :name, :doctor
   @@all = []
 
-  def self.all
-    @@all
-  end
+
 
   def initialize(name)
     @name = name
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def new_appointment(date, doctor)
