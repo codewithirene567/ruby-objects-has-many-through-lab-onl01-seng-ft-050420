5e8c2fde0b3e7b7b963c9ae150568887d0c7a026
Irene Scott <irenejinscott@gmail.com>
2020-05-22 17:55:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   end
 
   def appointments
-    appointment.all.select {|appointment| appointment.doctor == self}
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
   def patients
